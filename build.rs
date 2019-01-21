c38cd4ddeb1f33915d6b181f22b169ae38f67fc9
Trevor Spiteri <tspiteri@ieee.org>
2019-01-21 23:46:25 +0100

    fix build for rustc versions 1.17.0 and 1.18.0
    Include build.rs in Cargo.toml to make sure the 1.12.0 test uses
    build.rs during building. Then fix build.rs to be compatible with
    rustc version 1.12.0.

--- build.rs
+++ build.rs
@@ -1,12 +1,15 @@
 use std::env;
 use std::ffi::OsString;
+use std::io::{self, Write};
 use std::process::Command;
 
 fn main() {
     let version = match Version::read() {
         Ok(version) => version,
         Err(err) => {
-            eprintln!("failed to parse `rustc --version`: {}", err);
+            writeln!(&mut io::stderr(),
+                "failed to parse `rustc --version`: {}", err)
+                .unwrap();
             return;
         }
     };
@@ -57,7 +60,7 @@ impl Version {
             }
             num.push(c);
         }
-        let major = num.parse::<u32>().map_err(|e| e.to_string())?;
+        let major = try!(num.parse::<u32>().map_err(|e| e.to_string()));
 
         num.clear();
         for c in parts[1].chars() {
@@ -66,7 +69,7 @@ impl Version {
             }
             num.push(c);
         }
-        let minor = num.parse::<u32>().map_err(|e| e.to_string())?;
+        let minor = try!(num.parse::<u32>().map_err(|e| e.to_string()));
 
         num.clear();
         for c in parts[2].chars() {
@@ -75,8 +78,8 @@ impl Version {
             }
             num.push(c);
         }
-        let patch = num.parse::<u32>().map_err(|e| e.to_string())?;
+        let patch = try!(num.parse::<u32>().map_err(|e| e.to_string()));
 
-        Ok(Version { major, minor, patch })
+        Ok(Version { major: major, minor: minor, patch: patch })
     }
 }
